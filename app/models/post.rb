724d6936f227ac9eaa2cb365dd7053c7ead7b8d2
Monica  Karasek <mlkarasek@gmail.com>
2018-08-24 04:38:51 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true 
+  validates_presence_of :title
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
   validates :content, length: {minimum: 100}
 end
