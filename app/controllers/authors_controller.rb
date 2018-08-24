681fe69b800aedd84855547dbb0fd2c5524cc591
Monica  Karasek <mlkarasek@gmail.com>
2018-08-24 04:25:30 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,9 +8,13 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.create!(author_params)
-
+      if @post.valid?
+        @post.save 
     redirect_to author_path(@author)
-  end
+  else 
+    render :new
+  end 
+end
 
   private
 
